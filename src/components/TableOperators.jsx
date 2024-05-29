a3f3ac05b2d3bea0d6987f7b0086f5ed82d718b8
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2024-05-30 00:53:06 +0200

    more bugfixes

--- src/components/TableOperators.jsx
+++ src/components/TableOperators.jsx
@@ -10,18 +10,6 @@ export const TableOperators = ({ onDelete, role, operators = [] }) => {
             <table className="min-w-full divide-y divide-gray-200 table-fixed ">
               <thead className="bg-gray-100">
                 <tr>
-                  <th scope="col" className="p-4">
-                    <div className="flex items-center">
-                      <input
-                        id="checkbox-all"
-                        type="checkbox"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
-                      />
-                      <label htmlFor="checkbox-all" className="sr-only">
-                        checkbox
-                      </label>
-                    </div>
-                  </th>
                   <th
                     scope="col"
                     className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase"
@@ -52,29 +40,11 @@ export const TableOperators = ({ onDelete, role, operators = [] }) => {
                   >
                     Location
                   </th>
-                  <th
-                    scope="col"
-                    className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase"
-                  >
-                    Action
-                  </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
                 {operators.map((operator) => (
                   <tr key={operator.id} className="hover:bg-gray-100">
-                    <td className="p-4 w-4">
-                      <div className="flex items-center">
-                        <input
-                          id="checkbox-table-1"
-                          type="checkbox"
-                          className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600"
-                        />
-                        <label htmlFor="checkbox-table-1" className="sr-only">
-                          checkbox
-                        </label>
-                      </div>
-                    </td>
                     <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap ">
                       {operator.fname}
                     </td>
@@ -89,25 +59,6 @@ export const TableOperators = ({ onDelete, role, operators = [] }) => {
                     </td>
                     <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
                       {operator.location}
-                    </td>
-                    <td className="flex flex-row gap-1 py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
-                      <a
-                        href="#"
-                        className="text-gray-600 px-1 rounded-md bg-green-400 hover:underline"
-                      >
-                        View
-                      </a>
-                      {role === "admin" && (
-                        <>
-                          <a
-                            href="#"
-                            onClick={() => handleDelete(12)}
-                            className="text-gray-600 px-1 rounded-md bg-red-400 hover:underline"
-                          >
-                            Delete
-                          </a>
-                        </>
-                      )}
                     </td>
                   </tr>
                 ))}
